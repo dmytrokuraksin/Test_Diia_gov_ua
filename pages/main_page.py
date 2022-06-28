20f20c273341c118d40615fd892c23fa736b3864
iLnazhim <bass_boss@mail.ru>
2022-06-28 16:11:54 +0300

    Группировка тестов: setup

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,10 +9,10 @@ class MainPage(BasePage):
     # def should_be_login_link(self):
     #     #self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
     #     assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-    # def go_to_login_page(self):
-    #     login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-    #     login_link.click()
-    #     return LoginPage(browser=self.browser, url=self.browser.current_url)
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 
 

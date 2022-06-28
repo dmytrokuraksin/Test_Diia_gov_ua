fc18b3ee8385aa5731985ba114108917126c524b
iLnazhim <bass_boss@mail.ru>
2022-06-28 13:05:54 +0300

    Плюсы наследования

--- pages/locators.py
+++ pages/locators.py
@@ -16,3 +16,7 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
     PRICE_TOTAL = (By.CSS_SELECTOR, "#messages .alert-info strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert:nth-child(1)")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")

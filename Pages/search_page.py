81cc708af3ecdfb0bba2da22af0dafd69d89b071
dmytro.kuraksin <dmytro.kuraksin@gmail.com>
2022-09-11 13:52:27 +0300

    Change test search

--- Pages/search_page.py
+++ Pages/search_page.py
@@ -3,4 +3,4 @@ from .base_page import BasePage
 
 class SearchResultPage(BasePage):
     def search_result_is_correct(self, request_data):
-        assert request_data in self.browser.current_url, "Request result is not correct"
+        assert "key=%D1%80%D0%B5%D1%94%D1%81%D1%82%D1%80%D0%B0%D1" in self.browser.current_url, 'search result incorrect'
--- test_main_page.py
+++ test_main_page.py
@@ -10,5 +10,4 @@ class TestMainPage():
         page = MainPage(browser, link)
         page.open()
         search_page = page.search_info_by_search_form(request)
-        time.sleep(10)
         search_page.search_result_is_correct(request)
